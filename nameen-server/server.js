559b00d3bb172299b18336d4433e8a9dd91e9e84
Tim Sullivan <timbo.in.space@gmail.com>
2020-11-20 04:04:31 -0500

    Added buttons, progress bar

--- nameen-server/server.js
+++ nameen-server/server.js
@@ -17,23 +17,48 @@ const io = socketIo(server, {
 });
 
 let interval;
+const timePerTurn = 60.0;
+let timeRemaining = 60.0;
+
+const wordList = ['a bunch', 'of words', 'are all', 'in a list', 'one person', 'describes', 'the other', 'guesses'];
+let wordIndex = 0;
 
 io.on('connection', (socket) => {
+
   console.log("New client connected");
   if (interval) {
     clearInterval(interval);
   }
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  //interval = setInterval(() => getApiAndEmit(socket), 1000);
+  interval = setInterval(() => gameTimer(socket), 1000);
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
+
+  socket.on('GOT_WORD', () => {
+    console.log("Client got word, requesting another");
+    socket.emit('NEXT_WORD', wordList[(wordIndex++)%wordList.length]);
+  });
+
 });
 
 const getApiAndEmit = (socket) => {
   const response = new Date();
   // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
+  socket.emit("TICK", response);
+};
+
+const gameTimer = (socket) => {
+
+  timeRemaining--;
+
+  if (timeRemaining < 0)
+    timeRemaining = timePerTurn;
+
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("TICK", Math.floor(100 * timeRemaining / timePerTurn));
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
