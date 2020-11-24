a98847777183c13a8e088df24b2aa29099bad6f4
Tim Sullivan <timbo.in.space@gmail.com>
2020-11-24 09:09:25 -0500

    added sessions and separate games

--- nameen-server/server.js
+++ nameen-server/server.js
@@ -1,49 +1,88 @@
 const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
+const session = require('express-session');
 const port = 3001;
-const index = require("./routes/index");
-
 const app = express();
-app.use(index);
 
-const server = http.createServer(app);
-
-const io = socketIo(server, {
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
   cors: {
     origin: '*',
   }
+});
+
+const sessionMiddleware = session({
+  secret: 'squirrel',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: null },
+});
+app.use(sessionMiddleware);
+
+io.use((socket, next) => {
+  sessionMiddleware(socket.request, socket.request.res, next);
 });
 
 let interval;
 const timePerTurn = 60.0;
 let timeRemaining = 60.0;
 
-const wordList = ['Tom Cruise', 'Heptagon', 'Iggy Pop', 'Velco', 'Boa Constrictor', 'Some sports thing', 'Brian Mulroney', 'Diffraction'];
+const wordList = ['Tom Cruise', 'Heptagon', 'Iggy Pop', 'Velcro', 'Boa Constrictor', 'Some sports thing', 'Brian Mulroney', 'Diffraction'];
 let wordIndex = 0;
+
+let games = {}; // An array of game 
 
 io.on('connection', (socket) => {
 
-  console.log("New client connected");
+  console.log(`Client connected: ${socket.id}`);
+  const session = socket.request.session;
+  session.connections++;
+  session.save();
 
-  // Don't set a separate interval for each client
+  // Start the timer if it isnt going already
   if (interval) {
     clearInterval(interval);
   }
   interval = setInterval(() => gameTimer(socket), 1000);
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    console.log(`Client disconnected: ${socket.id}`);
     clearInterval(interval);
   });
 
-  socket.on('GOT_WORD', () => {
+  socket.on('GOT_WORD', (gameID) => {
+    // Double check that the socket is in the room
     console.log("Client got word, requesting another");
     socket.emit('NEXT_WORD', wordList[(wordIndex++)%wordList.length]);
   });
 
+  socket.on('JOIN_GAME', (gameID, playerName) => {
+    connectClientToGame(socket, gameID, playerName);
+  });
+
 });
+
+const connectClientToGame = (socket, gameID, playerName) => {
+  // Is the game already in the array?
+  if (!games.hasOwnProperty(gameID)) {
+    newGame = {
+      status:'waiting', 
+      teamA:[playerName], 
+      teamB:[], 
+      teamScores:[0,0]
+    };
+    games[gameID] = newGame;
+    console.log(`Client starting new game: ${gameID}.`);
+  } else { // Nope. Make a new entry
+    // Game exists. Which team has fewer players?
+    games[gameID].teamA.length > games[gameID].teamA.length ?
+    games[gameID].teamB.push(playerName) :
+    games[gameID].teamA.push(playerName) ;
+    console.log(`Client joining existing game: ${gameID}.`);
+  }
+  socket.join(gameID);
+  socket.to(gameID).emit('PLAYER_JOINED', games[gameID]);
+};
+
 
 const gameTimer = (socket) => {
 
@@ -55,5 +94,8 @@ const gameTimer = (socket) => {
   // Emitting a new message. Will be consumed by the client
   socket.emit("TICK", Math.floor(100 * timeRemaining / timePerTurn));
 };
+
+const index = require("./routes/index");
+app.use(index);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
